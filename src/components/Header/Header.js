06baf727622acdcc8a1e8b2dc1b6dec338b1a86e
Timo Santi <timo.santi@santicon.fi>
2022-03-02 15:24:11 +0200

    Desktop header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,62 +9,110 @@ import Logo from '../Logo';
 import Button from '../Button';
 
 const Header = () => {
-  return (
-    <header>
-      <SuperHeader>
-        <Row>
-          <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
-          </ActionGroup>
-          <ActionGroup>
-            <button>
-              <User size={24} />
-            </button>
-          </ActionGroup>
-        </Row>
-      </SuperHeader>
-      <MainHeader>
-        <Logo />
-      </MainHeader>
-    </header>
-  );
+    return (
+        <header>
+            <SuperHeader>
+                <Row>
+                    <SearchAndMenu />
+                    <UserMenu />
+                </Row>
+            </SuperHeader>
+            <MainHeader>
+                <ButtonWrapper>
+                    <SearchAndMenu />
+                </ButtonWrapper>
+                <Logo />
+                <Subscribe>
+                    <Button>Subscribe</Button>
+                    <a href="#">Already Subcriber?</a>
+                </Subscribe>
+            </MainHeader>
+        </header>
+    );
 };
 
+const Subscribe = styled.div`
+    display: none;
+
+    @media ${QUERIES.desktopAndUp} {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-self: end;
+    }
+`;
+
+const ButtonWrapper = styled.div`
+    display: none;
+
+    @media ${QUERIES.desktopAndUp} {
+        display: revert;
+    }
+`;
+
 const SuperHeader = styled.div`
-  padding: 16px 0;
-  background: var(--color-gray-900);
-  color: white;
+    padding: 16px 0;
+    background: var(--color-gray-900);
+    color: white;
+
+    @media ${QUERIES.desktopAndUp} {
+        display: none;
+    }
 `;
 
 const Row = styled(MaxWidthWrapper)`
-  display: flex;
-  justify-content: space-between;
+    display: flex;
+    justify-content: space-between;
 `;
 
 const ActionGroup = styled.div`
-  display: flex;
-  gap: 24px;
+    display: flex;
+    gap: 24px;
 
-  /*
+    /*
     FIX: Remove the inline spacing that comes with
     react-feather icons.
   */
-  svg {
-    display: block;
-  }
+    svg {
+        display: block;
+    }
 `;
 
 const MainHeader = styled(MaxWidthWrapper)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 32px;
-  margin-bottom: 48px;
+    align-items: center;
+    justify-content: center;
+    margin-top: 32px;
+    margin-bottom: 48px;
+    padding-left: 32px;
+    padding-right: 32px;
+
+    @media ${QUERIES.desktopAndUp} {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+    }
 `;
 
 export default Header;
+
+function SearchAndMenu(props) {
+    return (
+        <ActionGroup {...props}>
+            <button>
+                <Search size={24} />
+            </button>
+            <button>
+                <Menu size={24} />
+            </button>
+        </ActionGroup>
+    );
+}
+
+function UserMenu() {
+    return (
+        <ActionGroup>
+            <button>
+                <User size={24} />
+            </button>
+        </ActionGroup>
+    );
+}
